442b2031975190c7989ca0a4b76ef39ca5512366
bearachute <sjourand@gmail.com>
2020-05-21 03:17:21 -0400

    Trying out videogrep failing

---
+++ greppy/subby.py
@@ -0,0 +1,13 @@
+import re
+from moviepy.editor import VideoFileClip, concatenate
+
+video = VideoFileClip("C:/Users/owner/Desktop/greppy/Thundercats1.mp4")
+
+cuts = re.findall("mutants",text) != []
+
+def assemble_cuts(cuts, outfile):
+    final = concatenate([video.subclip(start,end)
+                       for (start,end) in cuts])
+    final.to_videofile(outfile)
+
+assemble_cuts(cuts, "mut.mp4")
